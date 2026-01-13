6fa6660c55762c4c8f156c9887ccbb7eac74f1e1
Poojasreekalepu <poojasreekalepu@gmail.com>
2026-01-13 10:11:49 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,14 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+        // 1. Declare two variables
+        int number1 = 10;
+        int number2 = 20;
+
+        // 2. Add the two numbers
+        int sum = number1 + number2;
+
+        // 3. Print the result
+        System.out.println("The sum is: " + sum);
+    }
+}
